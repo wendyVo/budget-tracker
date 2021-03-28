d0b1f0ab6906f151afac83ceebdc2651defaf72c
Wendy Uyen Vo <uyen199247@gmail.com>
2021-03-28 22:08:50 +1100

    deploying to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/style.css",
+    "/styles.css",
     "/index.js",
     "/db.js",
     "/manifest.webmanifest",
